104ce7f355d4ae5e23af2391a2a573ce8ac1f8b7
Harsha Shetty <142572142+harsha8876@users.noreply.github.com>
2024-05-12 00:19:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ try {
  }
 };
 
-app.use(cors({ origin: "https://outfsource.onrender.com", credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
